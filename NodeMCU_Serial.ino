0f662d780e435cc706537e42aeffa4cc026dc81f
mediumsitompul <44793858+mediumsitompul@users.noreply.github.com>
2020-09-03 23:20:51 +0700

    Create NodeMCU_Serial.ino
    File sketch

---
+++ NodeMCU_Serial.ino
@@ -0,0 +1,20 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  Serial.println("Hallo... ");
+  Serial.println("Hallo juga... ");
+  delay(3000);
+  
+  
+  Serial.println("Apa Kabar?");
+  Serial.println("Kabar Baik...");
+  Serial.println(" ");
+  
+  delay(3000);
+  
+}
